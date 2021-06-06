1eda45e820836bef0b69858897973d84cad6ad1e
ShotaroFukunaga <accelerator.engin@gmail.com>
2021-06-06 19:34:42 +0900

    ChangeBranch

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,10 +3,16 @@ class BoardsController < ApplicationController
     @boards = Board.includes(:user).order(created_at: :desc)
   end
 
-  def new
-  end
+  def new; end
 
   def create
+    @borad = current_user.boards.build(board_params)
+
+    if @board.save
+      redirect_to boards_path, success: t('.success')
+    else
+      render :index
+    end
   end
 
   def show
@@ -16,5 +22,9 @@ class BoardsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def board_params
+    params.require(:board).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   resources :users, only: %i[new create]
   resources :posts
-  resources :boards, only: %i[index]
+  resources :boards, only: %i[index new create]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
