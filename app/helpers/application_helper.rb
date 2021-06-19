8abac7dd946f657e2c06b51c54500f29b6736986
ShotaroFukunaga <accelerator.engin@gmail.com>
2021-06-19 11:49:49 +0900

    AddDynamicTitle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def page_title(page_titile = '')
+        base_title = 'RUNTEQ BOARD APP'
+
+        page_titile.empty? ? base_title : page_titile + "|" + base_title
+    end
 end
