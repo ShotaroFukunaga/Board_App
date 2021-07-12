74e47df172d854268b7be1165cb2817bc0d84fe2
ShotaroFukunaga <accelerator.engin@gmail.com>
2021-07-12 21:01:12 +0900

    DropTableToPosts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_060951) do
+ActiveRecord::Schema.define(version: 2021_07_12_115814) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
@@ -39,15 +39,6 @@ ActiveRecord::Schema.define(version: 2021_07_05_060951) do
     t.datetime "updated_at", null: false
     t.index ["board_id"], name: "index_comments_on_board_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
